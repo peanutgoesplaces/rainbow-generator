ce4ca218ab362899f1e82ea8a7b843456c5b50e3
peanutgoesplaces <130071637+peanutgoesplaces@users.noreply.github.com>
2026-08-31 19:36:57 -0400

    Update database.py

--- database.py
+++ database.py
@@ -6,15 +6,10 @@ load_dotenv()
 
 #Connection to SQL Database
 conn = psycopg2.connect(
-    #host="sakura.proxy.rlwy.net",
     host = os.getenv("DB_HOST"),
-    #port=31658,
     port = os.getenv("DB_PORT"),
-    #database='railway',
     database = os.getenv("DB_NAME"),
-    #user='postgres',
     user=os.getenv("DB_USER"),
-    #password='RxSaqrkMEfDTrmKrEDsVaHbPgUsxFIMR'
     password=os.getenv("DB_PASSWORD")
 )
 
